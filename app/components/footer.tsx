e26ab8486369ef2bd3c14d9aa82d0f074dbf3bd5
gra gra pow <155625311+unoxyzmdfckz@users.noreply.github.com>
2025-03-07 20:27:21 +0800

    Initial Commit

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -24,10 +24,10 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
+            href="https://ch4n-portfolio.vercel.app/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
+            <p className="ml-2 h-7">pf</p>
           </a>
         </li>
         <li>
