ac45d47a903bfa7cf9f39000f46dffe440ee7a1f
gra gra pow <155625311+unoxyzmdfckz@users.noreply.github.com>
2025-03-07 20:41:55 +0800

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Page() {
   return (
     <section>
@@ -6,8 +5,9 @@ export default function Page() {
         Our live website
       </h1>
       <p className="mb-4">
-        {`Welcome, Visitor! This is a live website that is built with Blitz.js. It's a fullstack React framework that is built on top of Next.js. 
-        Members: Gales, Chersan L., Padla, Jacqueline Zoe, and Saligumba, Dandy N.`}
+        {`Welcome to our live server. This is a live website that is built with Blitz.js. It's a fullstack React framework that is built on top of Next.js.`}
+        <br />
+        {`Members: Gales, Chersan L., Padla, Jacqueline Zoe, and Saligumba, Dandy N.`}
       </p>
     </section>
   )
