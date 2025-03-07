36808627a9d91732ecbb19b233b13098088392f2
gra gra pow <155625311+unoxyzmdfckz@users.noreply.github.com>
2025-03-07 20:32:11 +0800

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from './components/footer'
-import { baseUrl } from './sitemap'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -18,7 +17,6 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Our Portfolio',
     description: 'This is our portfolio.',
-    url: baseUrl,
     siteName: 'Our Portfolio',
     locale: 'en_US',
     type: 'website',
