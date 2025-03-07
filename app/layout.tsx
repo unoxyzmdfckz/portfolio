05ead2e61dd29c8fcab0b29011ecbd70ceff7df8
gra gra pow <155625311+unoxyzmdfckz@users.noreply.github.com>
2025-03-07 21:12:25 +0800

    dwada

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,9 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 const cx = (...classes) => classes.filter(Boolean).join(' ')
